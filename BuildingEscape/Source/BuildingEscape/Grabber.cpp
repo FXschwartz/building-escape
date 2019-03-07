45be74d3ff3ef18487ee060ff8eb307df1ecb5fd
FXschwartz <preston.schwartz505@gmail.com>
2019-03-06 20:53:03 -0600

    Start adding the ability to grab objects and carry them.

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -23,7 +23,30 @@ void UGrabber::BeginPlay()
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
-	
+
+	/// Look for attached physics handle
+	PhysicsHandle = GetOwner() -> FindComponentByClass<UPhysicsHandleComponent>();
+	if(PhysicsHandle) {
+		
+	}
+	else {
+		UE_LOG(LogTemp, Error, TEXT("Grabber BeginPlay() error: Cannot find PhysicsHandle from owner: %s"), *(GetOwner() -> GetName()));
+	}
+
+	/// Look for attached input component
+	InputComponent = GetOwner() -> FindComponentByClass<UInputComponent>();
+	if(InputComponent) {
+		UE_LOG(LogTemp, Warning, TEXT("InputComponent found"));
+		/// Bind input action
+		InputComponent -> BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+	}
+	else {
+		UE_LOG(LogTemp, Error, TEXT("Grabber BeginPlay() error: Cannot find InputComponent from owner: %s"), *(GetOwner() -> GetName()));
+	}
+}
+
+void UGrabber::Grab() {
+	UE_LOG(LogTemp, Warning, TEXT("Grab pressed!"));
 }
 
 
@@ -35,8 +58,6 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	FVector PlayerViewPointLocation;
 	FRotator PlayerViewPointRotation;
 	GetWorld() -> GetFirstPlayerController() -> GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
-
-	//UE_LOG(LogTemp, Warning, TEXT("location %s rotation %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
 
 	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
 	DrawDebugLine(
@@ -66,7 +87,7 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 		/// See what we hit
 	AActor* ActorHit = Hit.GetActor();
 	if(ActorHit) {
-		UE_LOG(LogTemp, Warning, TEXT("Line trace hit: %s"), *(ActorHit->GetName()));
+		// UE_LOG(LogTemp, Warning, TEXT("Line trace hit: %s"), *(ActorHit -> GetName()));
 	}
 }
 
