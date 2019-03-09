7122778ded2d33d0e0b34796dc6fc67c6df7cf75
FXschwartz <preston.schwartz505@gmail.com>
2019-03-09 12:55:52 -0600

    Added door animation and trigger plate sounds.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -25,28 +25,17 @@ void UOpenDoor::BeginPlay() {
 	}
 }
 
-void UOpenDoor::OpenDoor() {
-	Owner->SetActorRotation(FRotator(0.f, OpenAngle, 0.f));
-}
-
-void UOpenDoor::CloseDoor() {
-	Owner->SetActorRotation(FRotator(0.f, 0.f, 0.f));
-}
-
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// Poll the Trigger Volume
-	if(GetTotalMassOfActorsOnPlate() > 30.f) // TODO make into a parameter
-	{
-		OpenDoor();
-		LastDoorOpenTime = GetWorld() -> GetTimeSeconds();
+	if(GetTotalMassOfActorsOnPlate() > TriggerMass) { // TODO make into a parameter
+		OnOpenRequest.Broadcast();
 	}
 
-	// Check if it's time to close the door
-	if(GetWorld() -> GetTimeSeconds() - LastDoorOpenTime > DoorCloseDelay) {
-		CloseDoor();
+	else {
+		OnCloseRequest.Broadcast();
 	}
 }
 
@@ -65,7 +54,7 @@ float UOpenDoor::GetTotalMassOfActorsOnPlate() {
 	// Iterate through them adding their masses
 	for(const auto& Actor : OverlappingActors) {
 		TotalMass += Actor -> FindComponentByClass<UPrimitiveComponent>() -> GetMass();
-		UE_LOG(LogTemp, Warning, TEXT("%s on pressure plate"), *Actor -> GetName())
+		//UE_LOG(LogTemp, Warning, TEXT("%s on pressure plate"), *Actor -> GetName())
 	}
 
 	return TotalMass;
--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -3,9 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Runtime/Core/Public/Delegates/Delegate.h"
 #include "Components/ActorComponent.h"
 #include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDoorEvent);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
@@ -20,25 +23,24 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-	void OpenDoor();
-	void CloseDoor();
-
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-	
+
+	UPROPERTY(BlueprintAssignable)
+		FDoorEvent OnOpenRequest;
+
+	UPROPERTY(BlueprintAssignable)
+		FDoorEvent OnCloseRequest;
+
 private:
 	UPROPERTY(EditAnywhere)
-		float OpenAngle = -60.0f;
-
-	UPROPERTY(EditAnywhere)
 		ATriggerVolume* PressurePlate = nullptr;
-
-	UPROPERTY(EditAnywhere)
-		float DoorCloseDelay = 0.82f;
 	
-	float LastDoorOpenTime;
 	float GetTotalMassOfActorsOnPlate();
 
 	AActor* Owner = nullptr;
+
+	UPROPERTY(EditAnywhere)
+		float TriggerMass = 30.0f;
 };
